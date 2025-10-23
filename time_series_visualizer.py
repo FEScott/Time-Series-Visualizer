3a0515ac7fcb8bcdfde70866023791844e73a258
FEScott <freyascott32@gmail.com>
2025-10-23 19:05:15 +0000

    first commit

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -1,3 +1,4 @@
+from datetime import date
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -5,36 +6,51 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=True, index_col='date')
 
-# Clean data
-df = None
+# Set the index to the date column
+
+print(df.head())
+
+# Clean data: exclude top and bottom 2.5%
+df = df[
+    (df['value'] >= df['value'].quantile(0.025)) &
+    (df['value'] <= df['value'].quantile(0.975))
+]
+
 
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
+    fig = plt.figure()
+    plt.plot(df.index, df['value'], color='r', linewidth=1)
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    plt.ylabel('Page Views')
+    plt.xlabel('Date')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
 
 def draw_bar_plot():
-    # Copy and modify data for monthly bar plot
-    df_bar = None
+    # Copy and modify data for monthly bar 
+    df['month'] = df.index.month
+    df['year'] = df.index.year
+    df_bar = df.groupby(['month', 'year'])['value'].mean().unstack()
 
     # Draw bar plot
-
-
-
-
+    fig = plt.figure()
+    df_bar.plot(kind='bar')
+    plt.xlabel('Years')
+    plt.ylabel('Average Page Views')
+    plt.title('Average Page Views per month')
+    plt.legend(title='Month', labels=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'])
+    plt.show()
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
     return fig
+
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
